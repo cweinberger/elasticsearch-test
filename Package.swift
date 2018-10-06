61de40d6542a178324776d8e33d07fd80f80c76f
cweinberger <christian.weinberger@me.com>
2018-10-06 20:40:30 +0200

    add ES

--- Package.swift
+++ Package.swift
@@ -9,9 +9,11 @@ let package = Package(
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+
+        .package(url: "https://github.com/ryangrimm/VaporElasticsearch.git", .branch("feature/remove-whitespaces-in-folders"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Elasticsearch"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
